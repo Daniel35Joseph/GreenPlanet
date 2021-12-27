ede26a8d0bb99d5b44ada5701a79b29fa8ca13a7
Daniel35Joseph <87067443+Daniel35Joseph@users.noreply.github.com>
2021-12-27 16:56:19 +0200

    Creating Primary List Locations data model

---
+++ lib/data/list_locations.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_auth/Model/list_location.dart';
+
+final List<ListLocation> listLocations = [
+  ListLocation(
+    title: 'UFE',
+    urlImage:
+        'https://ufe.edu.eg/wp-content/uploads/2021/04/UFE-Logo-Icon-Blue.png',
+  ),
+  ListLocation(
+    title: 'BUE',
+    urlImage:
+        'https://upload.wikimedia.org/wikipedia/commons/a/a8/BUE_Logo.png',
+  ),
+  ListLocation(
+    title: 'ASU',
+    urlImage:
+        'https://1000logos.net/wp-content/uploads/2020/05/Arizona-State-Sun-Devils-Logo.jpg',
+  ),
+];
