5331b623b49727950eead098ff4f307830f61b99
Daniel35Joseph <87067443+Daniel35Joseph@users.noreply.github.com>
2022-02-02 07:28:20 +0200

    Setting up LocationScreen

---
+++ lib/Screens/Pages/locationScreen.dart
@@ -0,0 +1,87 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_auth/Model/list_location.dart';
+import 'package:flutter_auth/Screens/Pages/addLocation.dart';
+import 'package:flutter_auth/components/NavDrawer.dart';
+import 'package:flutter_auth/constants.dart';
+import 'package:flutter_auth/data/list_locations.dart';
+
+import 'background.dart';
+
+class LocationScreen extends StatefulWidget {
+  @override
+  _LocationsState createState() => _LocationsState();
+}
+
+class _LocationsState extends State<LocationScreen>{
+  Query _ref;
+
+  @override
+  void initState() {
+    super.initState();
+    _ref = FirebaseDatabase.instance.reference().child("Locations").orderByChild("title");
+  }
+
+  Widget _buildLocationItem({Map location}){
+    return Container(
+      margin: EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: kPrimaryLightColor,
+      ),
+      child: ListTile(
+        contentPadding: EdgeInsets.all(16),
+        leading: CircleAvatar(
+          radius: 32,
+          backgroundImage: NetworkImage(location['Image']),
+        ),
+        title: Text(
+          location['Title'],
+          style: TextStyle(fontSize: 20, color: Colors.black),
+        )
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      drawer: NavDrawer(),
+      appBar: AppBar(
+        title: Text('LOCATIONS'),
+        backgroundColor: kPrimaryColor,
+        centerTitle: true,
+      ),
+      body: Container(
+        height: double.infinity,
+        child: FirebaseAnimatedList(
+          query: _ref,
+          itemBuilder: (BuildContext context, DataSnapshot snapshot, Animation<double> animation, int index) {
+            Map location = snapshot.value;
+            return _buildLocationItem(
+              location: location
+            );
+          }
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: kPrimaryColor,
+        foregroundColor: Colors.white,
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return AddLocation(
+                title: "Insert Title Here",
+                );
+              },
+            ),
+          );
+        },
+      )
+    );
+  }
+}
