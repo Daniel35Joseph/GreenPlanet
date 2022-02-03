785adb40b7ef4c207246379b486b22d4b5b7641d
Daniel35Joseph <87067443+Daniel35Joseph@users.noreply.github.com>
2022-02-03 05:01:31 +0200

    Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_auth/constants.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 import 'Screens/Login_Registration/login_screen.dart';
 
@@ -7,10 +9,36 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  runApp(MyApp());
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+      seconds: 1,
+      navigateAfterSeconds: new AfterSplash(),
+      title: new Text(
+        'Mon Jardin',
+        style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 50.0, color: Colors.white),
+      ),
+      image: new Image.asset(
+          'assets/images/jardin_mon_logo-removebg-preview.png'),
+      backgroundColor: kPrimaryColor,
+      loaderColor: Colors.white,
+    );
+  }
+}
+
+class AfterSplash extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
